8b0d339bb7f7dda30621e9b416d136b9be99e026
Mohammad Afiq <46773936+afiqzx@users.noreply.github.com>
2024-07-04 20:03:19 +0800

    Better build.rs file (rebuild on template update)

--- build.rs
+++ build.rs
@@ -2,6 +2,9 @@ use std::process::Command;
 
 #[allow(dead_code)]
 fn main() {
+    // Only re-run if templates changed
+    println!("cargo:rerun-if-changed=./templates");
+
     let project_root = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     let input_css_path: String = [&project_root, "/css/input.css"].iter().map(|s| *s).collect();
     let output_css_path: String = [&project_root, "/css/output.css"].iter().map(|s| *s).collect();
@@ -10,4 +13,5 @@ fn main() {
         .args(["tailwindcss", "-i", &input_css_path, "-o", &output_css_path])
         .output()
         .expect("Failed to build tailwind");
+
 }
